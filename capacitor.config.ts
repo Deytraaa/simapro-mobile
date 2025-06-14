42a8224d83cc216906c6b530381d851064fa9639
Deytraaa <satriacahya162@gmail.com>
2025-06-14 18:13:38 +0700

    ganti nama app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'myApp',
+  appId: 'com.simapro.app',        // Bisa kamu ubah sesuai domain/app ID unikmu
+  appName: 'SIMAPRO',              // Ini yang tampil sebagai nama aplikasi
   webDir: 'dist'
 };
 
